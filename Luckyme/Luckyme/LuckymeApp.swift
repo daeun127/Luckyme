38f1bace95c31caed4e0295fd54b28be887ba372
daeun127 <68541900+daeun127@users.noreply.github.com>
2024-06-01 22:31:47 +0900

    Initial commit

---
+++ Luckyme/Luckyme/LuckymeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LuckymeApp.swift
+//  Luckyme
+//
+//  Created by DaeunLee on 6/1/24.
+//
+
+import SwiftUI
+
+@main
+struct LuckymeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
