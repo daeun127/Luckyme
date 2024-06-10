45398a34322c856846aaa1aca7807bda892b663a
daeun127 <68541900+daeun127@users.noreply.github.com>
2024-06-10 18:34:44 +0900

    🎉 change selected tabview icon

--- Luckyme/Luckyme/mainView.swift
+++ Luckyme/Luckyme/mainView.swift
@@ -10,39 +10,48 @@ import SwiftUI
 struct mainView: View {
     @Binding var mind: String
     @Binding var desc: String
-    
+    @State private var selectedTab = 0  // 현재 선택된 탭을 추적하기 위한 상태 변수
+
     var body: some View {
-        TabView{
+        TabView(selection: $selectedTab) {
             Home()
-                .tabItem{
-                    Image("home")
-                        
-                    Text("홈")
-                        .font(.system(size: 12.5, weight: .regular))
+                .tabItem {
+                    VStack{
+                        Image(selectedTab == 0 ? "home_c" : "home")
+                        Text("홈")
+                            .font(.system(size: 12.5, weight: .regular))
+                    }
                 }
+                .tag(0)
+
             worryBefore()
-                .tabItem{
-                    Image(systemName: "star")
+                .tabItem {
+                    Image(selectedTab == 1 ? "home_c" : "Change User")
                     Text("마인드 체인지")
                         .font(.system(size: 12.5, weight: .regular))
                 }
+                .tag(1)
+
             custom_mind(mindName: $mind, mindDesc: $desc)
-                .tabItem{
-                    Image(systemName: "star")
+                .tabItem {
+                    Image(selectedTab == 2 ? "home_c" : "Attitude")
                     Text("커스텀 마인드")
                         .font(.system(size: 12.5, weight: .regular))
                 }
+                .tag(2)
+
             lucky_card()
-                .tabItem{
-                    Image(systemName: "star")
+                .tabItem {
+                    Image(selectedTab == 3 ? "home_c" : "Clover")
                     Text("행운 부적")
                         .font(.system(size: 12.5, weight: .regular))
                 }
+                .tag(3)
         }
         .tint(.mainPink)
-            
     }
 }
+
 
 
 struct Header: View {
