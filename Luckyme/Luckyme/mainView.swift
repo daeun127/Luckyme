a03aa21073c71542e87493dd202c20a8392f6a0f
daeun127 <68541900+daeun127@users.noreply.github.com>
2024-06-11 16:00:24 +0900

    🎉 update tabview

--- Luckyme/Luckyme/mainView.swift
+++ Luckyme/Luckyme/mainView.swift
@@ -11,44 +11,44 @@ struct mainView: View {
     @Binding var mind: String
     @Binding var desc: String
     @State private var selectedTab = 0  // 현재 선택된 탭을 추적하기 위한 상태 변수
-
+    
+    
     var body: some View {
-        TabView(selection: $selectedTab) {
-            Home()
-                .tabItem {
-                    VStack{
+            TabView(selection: $selectedTab) {
+                Home()
+                    .tabItem {
                         Image(selectedTab == 0 ? "home_c" : "home")
                         Text("홈")
                             .font(.system(size: 12.5, weight: .regular))
                     }
-                }
-                .tag(0)
-
-            worryBefore()
-                .tabItem {
-                    Image(selectedTab == 1 ? "home_c" : "Change User")
-                    Text("마인드 체인지")
-                        .font(.system(size: 12.5, weight: .regular))
-                }
-                .tag(1)
-
-            custom_mind(mindName: $mind, mindDesc: $desc)
-                .tabItem {
-                    Image(selectedTab == 2 ? "home_c" : "Attitude")
-                    Text("커스텀 마인드")
-                        .font(.system(size: 12.5, weight: .regular))
-                }
-                .tag(2)
-
-            lucky_card()
-                .tabItem {
-                    Image(selectedTab == 3 ? "home_c" : "Clover")
-                    Text("행운 부적")
-                        .font(.system(size: 12.5, weight: .regular))
-                }
-                .tag(3)
-        }
-        .tint(.mainPink)
+                    .tag(0)
+                
+                worryBefore()
+                    .tabItem {
+                        Image(selectedTab == 1 ? "chm_c" : "chm")
+                        Text("마인드 체인지")
+                            .font(.system(size: 12.5, weight: .regular))
+                    }
+                    .tag(1)
+                
+                custom_mind(mindName: $mind, mindDesc: $desc)
+                    .tabItem {
+                        Image(selectedTab == 2 ? "cusm_c" : "cusm")
+                        Text("커스텀 마인드")
+                            .font(.system(size: 12.5, weight: .regular))
+                    }
+                    .tag(2)
+                
+                lucky_card()
+                    .tabItem {
+                        Image(selectedTab == 3 ? "cl_c" : "cl")
+                        Text("행운 부적")
+                            .font(.system(size: 12.5, weight: .regular))
+                    }
+                    .tag(3)
+            }
+            .tint(.mainPink)
+       
     }
 }
 
